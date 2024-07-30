a54a8d677328333610f6d1ae14aade3574df64fb
XiomaraHuamani <xiomara.huamani.c@gmail.com>
2024-07-30 10:45:21 -0500

    manejo de contenido

--- app/contact/contact-container.jsx
+++ app/contact/contact-container.jsx
@@ -10,8 +10,8 @@ const Contactcontainer = () => {
               <div className="contact__area-title">
                 <h3 className="mb-25">Ponerse en contacto</h3>
                 <p>
-                Conéctate con nuestros expertos y descubre asesoramiento 
-                inmobiliario exclusivo y personalizado.
+                  Conéctate con nuestros expertos y descubre asesoramiento
+                  inmobiliario exclusivo y personalizado.
                 </p>
               </div>
               <div className="contact__area-info">
@@ -22,7 +22,7 @@ const Contactcontainer = () => {
                   <div className="contact__area-info-item-content">
                     {/* <span>Emergency Help</span> */}
                     <h6>
-                      <a href="tel:+123(458)896895">+51 999999999</a>
+                      <a href="tel:+123(458)896895">+51 952934205</a>
                     </h6>
                   </div>
                 </div>
@@ -129,16 +129,16 @@ const Contactcontainer = () => {
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15830894606!2d-74.11976383964463!3d40.69766374865767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1652012644726!5m2!1sen!2sbd"
                   loading="lazy"
                 ></iframe> */}
-                
-              <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2675.4177301158065!2d-71.54855535896164!3d-16.395651837079768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424a5d014d3c69%3A0xd629054566e0c065!2sAv.%20Jos%C3%A9%20Abelardo%20Qui%C3%B1ones%2C%2004013!5e0!3m2!1ses!2spe!4v1722310197993!5m2!1ses!2spe" 
-                width="600" 
-                height="450" 
-                style={{ border: 0 }}
-                allowfullscreen="" 
-                loading="lazy" 
-                referrerpolicy="no-referrer-when-downgrade">
-              </iframe>
+
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2675.4177301158065!2d-71.54855535896164!3d-16.395651837079768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424a5d014d3c69%3A0xd629054566e0c065!2sAv.%20Jos%C3%A9%20Abelardo%20Qui%C3%B1ones%2C%2004013!5e0!3m2!1ses!2spe!4v1722310197993!5m2!1ses!2spe"
+                  width="600"
+                  height="450"
+                  style={{ border: 0 }}
+                  allowfullscreen=""
+                  loading="lazy"
+                  referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
               </div>
             </div>
           </div>
