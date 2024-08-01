11ad1ab9553632288c424091c0442e149f6c05cf
alberth castillo <alberth.castillo@tecsup.edu.pe>
2024-08-01 16:05:45 -0500

    cambios de blogs

--- app/blog-details/[id]/blog-comment.jsx
+++ app/blog-details/[id]/blog-comment.jsx
@@ -7,7 +7,7 @@ const Blogcomment = ({ blogsData }) => {
           <ul>
             <li>
               <a href="#">
-                <i className="fal fa-user"></i>By - Admin
+                <i className="fal fa-user"></i>By -{/*  {blogsData.postby} */}
               </a>
             </li>
             <li>
@@ -15,37 +15,28 @@ const Blogcomment = ({ blogsData }) => {
                 <i className="fal fa-calendar-alt"></i>07, March 2023
               </a>
             </li>
-            <li>
-              <a href="#">
-                <i className="fal fa-comments"></i>3 Comment
-              </a>
-            </li>
           </ul>
         </div>
         <h3 className="mb-20">{blogsData.title}</h3>
         <p className="mb-25">
-          Maecenas tincidunt hendrerit odio sed consectetur. Duis porta purus
-          sapien, eget pretium augue consectetur ut. Nunc nibh augue, pretium
-          quis imperdiet pellentesque, molestie eget nisi. Sed rutrum sit amet
-          eros ac egestas. Maecenas tincidunt dolor in massa iaculis, vitae
-          dignissim sem finibus. Pellentesque elementum vel arcu sit amet
-          rhoncus.
+          <div
+            className="webinar-description"
+            style={{ textAlign: "justify" }}
+            dangerouslySetInnerHTML={{ __html: blogsData.detalles }}
+          ></div>
         </p>
-        <p>
-          Nulla at eleifend lorem. Praesent et ex sed metus egestas feugiat.
-          Donec velit libero, feugiat ac dictum vel, dignissim id ante. Praesent
-          hendrerit posuere condimentum.
-        </p>
+        <p></p>
         <div className="blog__details-left-box">
           <div className="blog__details-left-box-icon">
             <img src="/img/icon/quote.png" alt="" />
           </div>
           <p>
-            Aenean imperdiet finibus sodales. Sed non ex nisl. Maecenas ut
-            dictum neque, at euismod felis. Etiam rhoncus neque vitae efficitur
-            mollis. Vestibulum sed pulvinar magna. Suspendisse
+            En Quinta Tawa, tu seguridad es nuestra prioridad. Verifica todos
+            los detalles y consulta con nuestros expertos para asegurar una
+            inversión segura y confiable. Tu precaución garantiza una compra sin
+            sorpresas.{" "}
           </p>
-          <h5>David Beckham</h5>
+          <h5>Nils Castillo </h5>
         </div>
         <p>
           Vestibulum eget tellus rhoncus, dictum massa a, mattis massa. Cras in
@@ -76,98 +67,6 @@ const Blogcomment = ({ blogsData }) => {
           malesuada fames ac ante ipsum primis in faucibus. Integer tristique
           sem eget leo faucibus porttitor.
         </p>
-        <div className="blog__details-left-comment mb-45">
-          <h4 className="mb-40">Comment (2)</h4>
-          <div className="blog__details-left-comment-item">
-            <div className="blog__details-left-comment-item-comment">
-              <div className="blog__details-left-comment-item-comment-image">
-                <img src="/img/avatar/comment-1.jpg" alt="" /> /
-              </div>
-              <div className="blog__details-left-comment-item-comment-content">
-                <h5>
-                  Justin Bieber
-                  <a href="#">
-                    <i className="far fa-reply-all"></i>Reply
-                  </a>
-                </h5>
-                <span>20 May, 2023 At 9:PM</span>
-                <p>
-                  Phasellus nisi sapien, rutrum placerat sapien eu, rhoncus
-                  tempus felis. Nulla non pulvinar enim, vel viverra nunc.
-                  Integer condimentum vulputate justo.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="blog__details-left-comment-item ml-65 sm-ml-0">
-            <div className="blog__details-left-comment-item-comment">
-              <div className="blog__details-left-comment-item-comment-image">
-                <img src="/img/avatar/comment-2.jpg" alt="" />
-              </div>
-              <div className="blog__details-left-comment-item-comment-content">
-                <h5>
-                  Camila Cabello
-                  <a href="#">
-                    <i className="far fa-reply-all"></i>Reply
-                  </a>
-                </h5>
-                <span>22 May, 2023 At 7:PM</span>
-                <p>
-                  Phasellus nisi sapien, rutrum placerat sapien eu, rhoncus
-                  tempus felis. Nulla non pulvinar enim, vel viverra nunc.
-                  Integer condimentum vulputate justo.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="blog__details-left-contact">
-          <h4 className="mb-40">Add Comment</h4>
-          <div className="blog__details-left-contact-form">
-            <form action="#">
-              <div className="row">
-                <div className="col-sm-6 mb-30">
-                  <div className="blog__details-left-contact-form-item">
-                    <i className="fal fa-user"></i>
-                    <input type="text" name="name" placeholder="Full Name" />
-                  </div>
-                </div>
-                <div className="col-sm-6 sm-mb-30">
-                  <div className="blog__details-left-contact-form-item">
-                    <i className="fal fa-envelope"></i>
-                    <input
-                      type="text"
-                      name="email"
-                      placeholder="Email Address"
-                    />
-                  </div>
-                </div>
-                <div className="col-sm-12 mb-30">
-                  <div className="blog__details-left-contact-form-item">
-                    <i className="fal fa-globe"></i>
-                    <input type="text" name="subject" placeholder="https://" />
-                  </div>
-                </div>
-                <div className="col-sm-12 mb-30">
-                  <div className="blog__details-left-contact-form-item">
-                    <i className="fal fa-pen"></i>
-                    <textarea
-                      name="message"
-                      placeholder="Type your comments...."
-                    ></textarea>
-                  </div>
-                </div>
-                <div className="col-lg-12">
-                  <div className="blog__details-left-contact-form-item">
-                    <button className="theme-btn" type="submit">
-                      post Comment<i className="fal fa-long-arrow-right"></i>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
       </div>
     </div>
   );
