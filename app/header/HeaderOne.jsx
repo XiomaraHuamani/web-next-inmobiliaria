585178a31129e98de16139d2c14a055f0b800995
alberth castillo <alberth.castillo@tecsup.edu.pe>
2024-08-03 11:22:17 -0500

    cambios de header one

--- app/header/HeaderOne.jsx
+++ app/header/HeaderOne.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
 import { Blog, Home, Page, Room } from "./Menu";
@@ -7,80 +7,94 @@ import Social from "../socials/page";
 import Link from "next/link";
 
 export default function HeaderOne({ variant }) {
-    const [mobileToggle, setMobileToggle] = useState(false);
-    const [isSticky, setIsSticky] = useState(false);
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 0) {
-                setIsSticky(true);
-            } else {
-                setIsSticky(false);
-            }
-        });
-    }, []);
+  const [mobileToggle, setMobileToggle] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    });
+  }, []);
 
-    return (
-        <div className={`header__sticky ${variant ? variant : ""} ${isSticky ? "header__sticky-sticky-menu" : ""}`}>
-            <div className='header__area'>
-                <div className="container custom__container">
-                    <div className="header__area-menubar">
-                        <div className="header__area-menubar-left">
-                            <div className="header__area-menubar-left-logo">
-                                <Link href="/">
-                                    <img
-                                        src="/navlogo.svg"
-                                        alt="logo"
-
-                                    />
-                                </Link>
-                                <span className={mobileToggle ? "mobile-menu mobile-menu-active" : "mobile-menu"} onClick={() => setMobileToggle(!mobileToggle)}><span></span></span>
-                            </div>
-                        </div>
-                        <div className="header__area-menubar-right">
-                            <div className="header__area-menubar-right-menu menu-responsive">
-                                <ul className="mobile__menu" style={{ display: `${mobileToggle ? "block" : "none"}` }}>
-
-                                    <li className="menu-item-has-children"><a href="/">Inicio</a>
-                                        <DropDown />
-                                        {/* <ul className="sub-menu">
+  return (
+    <div
+      className={`header__sticky ${variant ? variant : ""} ${
+        isSticky ? "header__sticky-sticky-menu" : ""
+      }`}
+    >
+      <div className="header__area">
+        <div className="container custom__container">
+          <div className="header__area-menubar">
+            <div className="header__area-menubar-left">
+              <div className="header__area-menubar-left-logo">
+                <Link href="/">
+                  <img src="/navlogo.svg" alt="logo" />
+                </Link>
+                <span
+                  className={
+                    mobileToggle
+                      ? "mobile-menu mobile-menu-active"
+                      : "mobile-menu"
+                  }
+                  onClick={() => setMobileToggle(!mobileToggle)}
+                >
+                  <span></span>
+                </span>
+              </div>
+            </div>
+            <div className="header__area-menubar-right">
+              <div className="header__area-menubar-right-menu menu-responsive">
+                <ul
+                  className="mobile__menu"
+                  style={{ display: `${mobileToggle ? "block" : "none"}` }}
+                >
+                  <li className="menu-item-has-children">
+                    <a href="/">Inicio</a>
+                    <DropDown />
+                    {/* <ul className="sub-menu">
                                             <Home />
                                         </ul> */}
-                                    </li>
-                                    <li className="menu-item-has-children"><a href="/">Propiedades</a>
-                                        <DropDown />
-                                        <ul className="sub-menu">
-                                            <Page />
-                                        </ul>
-                                    </li>
-                                    {/* <li className="menu-item-has-children"><a href="/room-modern">Proyectos</a>
+                  </li>
+                  <li className="menu-item-has-children">
+                    <a href="/">Propiedades</a>
+                    <DropDown />
+                    <ul className="sub-menu">
+                      <Page />
+                    </ul>
+                  </li>
+                  {/* <li className="menu-item-has-children"><a href="/room-modern">Proyectos</a>
                                         <DropDown />
                                         <ul className="sub-menu">
                                             <Room />
                                         </ul> 
                                     </li> */}
-                                    <li className="menu-item-has-children"><a href="/blog-grid">Blog</a>
-                                        <DropDown />
-                                        {/* <ul className="sub-menu">
+                  <li className="menu-item-has-children">
+                    <a href="/blog-grid">Blog</a>
+                    <DropDown />
+                    {/* <ul className="sub-menu">
                                             <Blog />
                                         </ul> */}
-                                    </li>
-                                    <li >
-                                        <Link href="/contact">Contacto</Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="header__area-menubar-right-box">
-                            {/*       <div className="header__area-menubar-right-box-btn">
+                  </li>
+                  <li>
+                    <Link href="/contact">Contacto</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="header__area-menubar-right-box">
+              {/*       <div className="header__area-menubar-right-box-btn">
                                 <Link className="theme-btn" href="/contact">Contactanos<i className="fal fa-long-arrow-right"></i></Link>
                             </div> */}
-                            <div className="footer__area-widget-about-social text-white">
-                                <Social />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              <div className="footer__area-widget-about-social text-white">
+                <Social />
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
